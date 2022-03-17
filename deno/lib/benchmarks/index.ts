6b06f63bad19caa36176e6da186fd7e1ce117d19
Tom MacWright <tom@macwright.org>
2022-03-17 19:42:53 -0400

    Faster unions

--- deno/lib/benchmarks/index.ts
+++ deno/lib/benchmarks/index.ts
@@ -1,12 +1,14 @@
 import discriminatedUnionBenchmarks from "./discriminatedUnion.ts";
 import objectBenchmarks from "./object.ts";
+import primitiveBenchmarks from "./primitives.ts";
 import stringBenchmarks from "./string.ts";
 import unionBenchmarks from "./union.ts";
 
 for (const suite of [
+  ...unionBenchmarks.suites,
+  ...primitiveBenchmarks.suites,
   ...stringBenchmarks.suites,
   ...objectBenchmarks.suites,
-  ...unionBenchmarks.suites,
   ...discriminatedUnionBenchmarks.suites,
 ]) {
   suite.run();
--- src/benchmarks/index.ts
+++ src/benchmarks/index.ts
@@ -1,12 +1,14 @@
 import discriminatedUnionBenchmarks from "./discriminatedUnion";
 import objectBenchmarks from "./object";
+import primitiveBenchmarks from "./primitives";
 import stringBenchmarks from "./string";
 import unionBenchmarks from "./union";
 
 for (const suite of [
+  ...unionBenchmarks.suites,
+  ...primitiveBenchmarks.suites,
   ...stringBenchmarks.suites,
   ...objectBenchmarks.suites,
-  ...unionBenchmarks.suites,
   ...discriminatedUnionBenchmarks.suites,
 ]) {
   suite.run();
